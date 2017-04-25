58d666061a1b0515491c10744e7de5a473acd131
mothermoose <melanie.ilseman@gmail.com>
2017-04-24 21:37:52 -0500

    item 1

---
+++ store_item.rb
@@ -0,0 +1,21 @@
+class Springcollection
+  attr_reader :color, :price, :brand
+
+  def initialize(input_options)
+    @color = input_options[:color]
+    @price = input_options[:price]
+    @brand = input_options[:brand]
+  end 
+end 
+
+  item_1 = Springcollection.new(
+    color: "red", 
+    price: 1, 
+    brand: "addidas"
+  ) 
+
+  puts item_1.color
+  puts item_1.price
+  puts item_1.brand 
+
+
